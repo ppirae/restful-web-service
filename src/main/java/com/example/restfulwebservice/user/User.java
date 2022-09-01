c5d59834e060666c9e8fd55fe53435d28bcd70af
Hyeongrae Jo <76774733+ppirae@users.noreply.github.com>
2022-09-01 16:03:42 +0900

    #4 Spring Boot API 사용 (Swagger, security)

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -2,6 +2,8 @@ package com.example.restfulwebservice.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,18 +16,23 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 //@JsonIgnoreProperties(value = {"password", "ssn"})
-@JsonFilter("UserInfo")
+//@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
 
     private Integer id;
 
     @Size(min=2, message = "Name은 2글자 이상 입력해 주세요. ")
+    @ApiModelProperty(notes = "사용자 이름을 입력해주세요.")
     private String name;
 
     @Past   //과거 데이터만 올 수 있음
+    @ApiModelProperty(notes = "사용자의 등록일을 입력해주세요.")
     private LocalDateTime joinDate;
 
+    @ApiModelProperty(notes = "사용자 패스워드를 입력해주세요.")
     private String password;
 
+    @ApiModelProperty(notes = "사용자 주민번호를 입력해주세요.")
     private String ssn;
 }
